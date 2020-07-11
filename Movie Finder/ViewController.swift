05f298b642fc1e923372995d6f3f2a6d14e4b797
Rahman <59799545+rahman-aj@users.noreply.github.com>
2020-07-11 11:09:09 +0800

    Initial Commit

---
+++ Movie Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Finder
+//
+//  Created by Abdalrahman Aboujeeb on 11/07/2020.
+//  Copyright © 2020 Abdalrahman Aboujeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
